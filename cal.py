8b9ca4dcee47de40c89f19adde3e0b54751e1b7d
RonClub <99624950+Ronpogi15@users.noreply.github.com>
2022-07-19 23:09:02 +0800

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,64 @@
+print("")
+print("facebook:https://www.facebook.com/Cybersec15")
+
+print("")
+
+
+print("[1]SUBTRACTION")
+print("[2]ADDITION")
+print("[3]MULTIPLICATION")
+print("[4]DIVISION")
+print("[5]MODULUS")
+print("[6]FLOOR DIVISION")
+print("[7]EXPONET")
+
+pick = input("ENTER : ")
+
+if pick == "1":
+    number1 = input("ENTER NUMBER : ")
+    number2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(number1) - int(number2)
+    print(number1 + " - " + number2 + " = " +  str(result))
+    quit()
+
+elif pick == "2":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) + int(num2)
+    print(num1 + " + " + num2 + " = " + str(result))
+    quit()
+
+elif pick == "3":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) * int(num2)
+    print(num1 + " * " + num2 + " = " + str(result))
+    quit()
+
+elif pick == "4":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) / int(num2)
+    print(num1 + " / " + num2 + " = " + str(result))
+    quit()
+
+elif pick == "5":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) % int(num2)
+    print(num1 + " % " + num2 + " = " + str(result))
+    quit()
+
+elif pick == "6":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) // int(num2)
+    print(num1 + " // " + num2 + " = " + str(result))
+    quit()
+
+if pick == "7":
+    num1 = input("ENTER NUMBER : ")
+    num2 = input("ENTER ANOTHER NUMBER : ")
+    result = int(num1) ** int(num2)
+    print(num1 + " ** " + num2 + " = " + str(result))
+    exit()
